0fa0f071c1f3d222fea3410f4bb110bd9897b664
hideshis <hideshis1990@gmail.com>
2016-06-05 12:22:11 +0900

    added FizzBuzz and its test.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans = fizzBuzz(15);
+            Console.WriteLine(ans);
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        public static string fizzBuzz(int i)
+        {
+            //3の倍数でかつ5の倍数の場合は"FizzBuzz"を出力
+            if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
+
+            //3の倍数の倍数の場合は"Fizz"を出力
+            if (i % 3 == 0) return "Fizz";
+
+            //5の倍数の場合は"Buzz"を出力
+            if (i % 5 == 0) return "Buzz";
+
+            //それ以外の場合はその数値をそのまま文字列として出力
+            return i.ToString();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1Tests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void fizzBuzzTest()
+        {
+            string ans = Program.fizzBuzz(15);
+            Equals(ans, "FizzBuzz");
+        }
+    }
+}
