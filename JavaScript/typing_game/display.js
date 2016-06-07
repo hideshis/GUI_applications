71f46045593212e30a13c4b56dc177dad6572b36
hideshis <hideshis1990@gmail.com>
2016-06-07 23:37:10 +0900

    created Target class and Score class.

--- JavaScript/typing_game/display.js
+++ JavaScript/typing_game/display.js
@@ -1,16 +1,32 @@
-function initilaDisplay() {
+class Target {
+    constructor () {
+        this.target = "hogehoge.com"
+    }
+}
+
+class Score {
+    constructor () {
+        this.score = 0
+    }
+}
+
+function initilaDisplay(target, scoreLabel, missLabel) {
     'use strict';
 
     var currentWord = 'apple';
     var currentLocation = 0;
     var score = 0;
     var miss = 0;
-    var target = document.getElementById('target').innerText = "hogehoge.com";
-    var scoreLabel = document.getElementById('score').innerText = 0;
-    var missLabel = document.getElementById('miss').innerText = 0;
+    var target = document.getElementById('target').innerText = target;
+    var scoreLabel = document.getElementById('score').innerText = scoreLabel;
+    var missLabel = document.getElementById('miss').innerText = missLabel;
 };
 
-initilaDisplay();
+var target = new Target();
+var scoreLabel = new Score();
+var missLabel = new Score();
+
+initilaDisplay(target.target, scoreLabel.score, missLabel.score);
 
 document.onkeypress = function (e) {
     var element = document.createElement('p');
