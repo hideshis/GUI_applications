719b3c18ff9b741bac5393ad2d80b79407de4003
hideshis <hideshis1990@gmail.com>
2016-06-07 18:38:14 +0900

    created initialDisplay() function.

--- JavaScript/typing_game/display.js
+++ JavaScript/typing_game/display.js
@@ -1,11 +1,13 @@
-(function() {
+function initilaDisplay() {
     'use strict';
 
     var currentWord = 'apple';
     var currentLocation = 0;
     var score = 0;
     var miss = 0;
-    var target = document.getElementById('target');
-    var scoreLabel = document.getElementById('score');
-    var missLabel = document.getElementById('miss');
-})();
+    var target = document.getElementById('target').innerText = "hogehoge.com";
+    var scoreLabel = document.getElementById('score').innerText = 0;
+    var missLabel = document.getElementById('miss').innerText = 0;
+};
+
+initilaDisplay();
