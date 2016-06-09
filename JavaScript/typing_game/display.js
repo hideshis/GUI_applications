9b072bd62e41b1956c7dca2ee23c13b06abcaf20
hideshis <hideshis1990@gmail.com>
2016-06-09 15:34:47 +0900

    Added typing speed.

--- JavaScript/typing_game/display.js
+++ JavaScript/typing_game/display.js
@@ -29,31 +29,50 @@ class Score {
     }
 }
 
+class Speed {
+    constructor() {
+        this.time = 0;
+        this.numTyped = 0;
+    }
+
+    getSpeed() {
+        return (this.numTyped / this.time).toFixed(2);
+    }
+}
+
 String.prototype.replaceAt=function(index, character) {
     return this.substr(0, index) + character + this.substr(index+character.length);
 }
 
-function Display (target, scoreLabel, missLabel) {
+function Display (target, scoreLabel, missLabel, typingSpeed) {
     'use strict';
 
     var target = document.getElementById('target').textContent = target;
     var scoreLabel = document.getElementById('score').textContent = scoreLabel;
     var missLabel = document.getElementById('miss').textContent = missLabel;
+    var typingSpeed = document.getElementById('speed').textContent = typingSpeed;
 };
 
 document.onkeypress = function (e) {
     if (e.key == target.checkingTargetChar) {
+        typingSpeed.numTyped += 1;
         target.updateTargetAndLocation();
         scoreLabel.updateScore();
-        Display(target.target, scoreLabel.score, missLabel.score);
+        Display(target.target, scoreLabel.score, missLabel.score, typingSpeed.getSpeed());
     } else {
         missLabel.updateScore();
-        Display(target.target, scoreLabel.score, missLabel.score);
+        Display(target.target, scoreLabel.score, missLabel.score, typingSpeed.getSpeed());
     }
 };
+
+setInterval(function () {
+    typingSpeed.time += 0.001;
+    Display(target.target, scoreLabel.score, missLabel.score, typingSpeed.getSpeed());
+},1);
 
 var target = new Target();
 var scoreLabel = new Score();
 var missLabel = new Score();
+var typingSpeed = new Speed();
 
-Display(target.target, scoreLabel.score, missLabel.score);
+Display(target.target, scoreLabel.score, missLabel.score, typingSpeed.getSpeed());
